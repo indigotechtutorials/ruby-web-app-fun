7401d36e7884d794448e22a8c491be96da938973
Enoch Tamulonis <enochtamulonis@gmail.com>
2025-11-17 17:16:40 +0700

    added rails type of routes handling with root setting to controller and action

--- index.rb
+++ index.rb
@@ -1,13 +1,28 @@
 require 'webrick'
 require 'pry'
-require_relative "./controllers/pages_controller"
+# require_relative "./controllers/pages_controller"
 
-root = File.expand_path './views'
 server = WEBrick::HTTPServer.new Port: 8000
 
-server.mount_proc '/' do |req, res|
-  controller = PagesController.new(res)
-  controller.home
+Dir[File.join(__dir__, 'controllers', '*.rb')].each { |file| require file }
+
+routes_file = "./config/routes.rb"
+
+File.readlines(routes_file, chomp: true).each do |line|
+  route_action = line.split(" ")[0]
+  controller_mapping = line.split(" ")[1].gsub(/\"/, "")
+  controller_name = controller_mapping.split("#")[0]
+  action_name = controller_mapping.split("#")[1]
+  
+  served_url = case route_action
+  when "root"
+    "/"
+  end
+  server.mount_proc served_url do |req, res|
+    controller_class = Object.const_get("#{controller_name.capitalize}Controller")
+    controller = controller_class.new(res)
+    controller.send(action_name)
+  end
 end
 
 trap 'INT' do server.shutdown end
